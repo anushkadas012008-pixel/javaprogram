9a8d3225715c4f1d865cb2f548570a0f51b3686d
anushkadas <anushkadas012008@gmail.com>
2026-06-12 20:53:39 +0530

    new program

---
+++ javabasics.java
@@ -0,0 +1,9 @@
+public class javabasics{
+    public static void main(String args[])
+    {
+        System.out.println("hi paji\nhow are you");
+        int a=10;
+        int c=20;
+        System.out.println(a+c);
+    }
+}
