55de76c95d87be2f1eb41eada5e5f1bc4cfa13b4
anushkadas <anushkadas012008@gmail.com>
2026-06-13 15:45:54 +0530

    number is posituve or negative

---
+++ p5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class p5{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        if(a>0)
+            System.out.println("number is positive");
+        else if(a<0)
+            System.out.println("number is negative");
+        else
+            System.out.println("number is zero");
+        sc.close();
+    }
+}
