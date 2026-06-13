72c8a55951d84628719d6a1ac8ddee8e07bd60fb
anushkadas <anushkadas012008@gmail.com>
2026-06-13 15:56:49 +0530

    leap year

---
+++ p7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class p7{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        if(a%4==0 && a%100!=0 || a%400==0)
+        {
+            System.out.println("leap year");
+        }
+        else
+        {
+            System.out.println("not a leap year");
+        }
+        sc.close();
+    }
+}
